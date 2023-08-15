f5659814b07900bd578cb093701314d5fd0ebfec
Marius Ziegenbein <ziegenbeinmarius@gmail.com>
2023-08-15 21:42:56 +0200

    Vercel test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home() {
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center">
         <Button onClick={toastBoi}>Hello</Button>
+        <p className="text-2xl ">Some text</p>
         <p className="text-2xl ">
           {hello.data ? hello.data.greeting : "Loading tRPC query..."}
         </p>
