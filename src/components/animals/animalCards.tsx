1a2cae99502ea5687a021a0b93a87d4e7260b71f
Marius Ziegenbein <ziegenbeinmarius@gmail.com>
2023-09-03 20:32:50 +0200

    Adjust animal grid responsiveness

--- src/components/animals/animalCards.tsx
+++ src/components/animals/animalCards.tsx
@@ -57,7 +57,7 @@ export const AnimalCards: React.FC<AnimalCardsProps> = () => {
   return (
     <div className="space-y-4">
       <h2>Our animals</h2>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {animals.map((animal) => {
           return <AnimalCard animal={animal} key={animal.id} />;
         })}
