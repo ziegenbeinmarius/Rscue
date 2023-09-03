d6407fc0152825e2ff022e24a9a7bcabddd10df7
Marius Ziegenbein <ziegenbeinmarius@gmail.com>
2023-09-03 18:36:47 +0200

    RSC-2 improve the header

--- src/components/layouts/root.tsx
+++ src/components/layouts/root.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "../ui/navigation-menu";
@@ -12,6 +13,16 @@ import { Button } from "../ui/button";
 
 import { Raleway } from "next/font/google";
 import { cn } from "@/utils";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 const raleway = Raleway({ subsets: ["latin"] });
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -19,6 +30,19 @@ interface RootLayoutProps {
 
 export const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   const { data: sessionData } = useSession();
+
+  // Handles the event of header being on top of page to make bg transparent
+  const [atTop, setAtTop] = useState(true);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setAtTop(window.scrollY === 0 ? true : false);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [atTop]);
+
   return (
     <>
       <Head>
@@ -27,36 +51,46 @@ export const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={cn(raleway.className)}>
-        <nav className="sticky top-0 z-[99] h-12 bg-secondary p-4 sm:h-24  sm:px-8  sm:py-4 ">
+        <nav
+          className={cn(
+            "sticky top-0 z-[99] p-4 transition-all duration-300",
+            atTop ? " sm:px-8 sm:py-4" : "bg-secondary  sm:px-8 sm:py-4"
+          )}
+        >
           <div className="m-auto flex max-w-screen-2xl flex-row items-center justify-between ">
             <h2>Rscue</h2>
-            <NavigationMenu>
-              <NavigationMenuList>
-                {/* <NavigationMenuItem>
-                  <p>hek</p>
-                </NavigationMenuItem> */}
+            <NavigationMenu className="z-[1000]">
+              <NavigationMenuList className="gap-8">
+                <NavigationMenuItem className="underline-offset-2 hover:underline">
+                  <Link href="/about" legacyBehavior passHref>
+                    <NavigationMenuLink>About us</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
                 <NavigationMenuItem>
-                  <NavigationMenuTrigger>
-                    {sessionData ? (
-                      <span>Hey {sessionData.user?.name}</span>
-                    ) : (
-                      <span>Login</span>
-                    )}
-                  </NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <div className="p-4">
-                      <Button
-                        className=""
-                        onClick={
-                          sessionData
-                            ? () => void signOut()
-                            : () => void signIn()
-                        }
-                      >
-                        {sessionData ? "Sign out" : "Sign in"}
-                      </Button>
-                    </div>
-                  </NavigationMenuContent>
+                  {sessionData ? (
+                    <DropdownMenu>
+                      <DropdownMenuTrigger className="flex flex-row gap-1 text-primary underline-offset-2 hover:underline">
+                        <span>Hey {sessionData.user?.name}</span>
+                        <ChevronDown></ChevronDown>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="z-[1000]">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                          className="bg-primary text-primary-foreground hover:bg-primary/90 focus:bg-primary/90 focus:text-primary-foreground"
+                          onClick={() => void signOut()}
+                        >
+                          Sign out
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  ) : (
+                    <Button className="" onClick={() => void signIn()}>
+                      Sign in
+                    </Button>
+                  )}
                 </NavigationMenuItem>
               </NavigationMenuList>
             </NavigationMenu>
